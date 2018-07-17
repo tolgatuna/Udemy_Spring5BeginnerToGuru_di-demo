59cf49163cf5a1648daaf86371fa02943f136d0a
tolgatuna <tolga.tuna@etstur.com>
2018-07-17 17:54:58 +0300

    30. The Spring Framework Context

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -1,12 +1,18 @@
 package guru.springframework;
 
+import guru.springframework.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiDemoApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(DiDemoApplication.class, args);
+
+        // We not call any "new", spring has already create a MyController (Because it is annotated with @Controller)
+        MyController createdControllerFromSpring = (MyController) context.getBean("myController");
+        createdControllerFromSpring.hello();
     }
 }
---
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -0,0 +1,10 @@
+package guru.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public void hello() {
+        System.out.println("Hello!!!!");
+    }
+}
