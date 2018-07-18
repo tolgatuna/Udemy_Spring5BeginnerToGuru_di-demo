6901d2bf34e24f459265e12675ce9188afccd654
tolgatuna <tolga.tuna@etstur.com>
2018-07-18 16:50:47 +0300

    36. Spring Profiles

--- src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Primary    // It will be default service!
-@Profile("en")
-public class PrimaryGreetingService implements GreetingService {
+@Primary
+@Profile("es") //Spanish Primary Profile
+public class PrimarySpanishGreetingService implements GreetingService{
     @Override
     public String sayGreeting() {
-        return "Hello - I am the PRIMARY GREETING SERVICE";
+        return "Servicio de Saludo Primario";
     }
 }
