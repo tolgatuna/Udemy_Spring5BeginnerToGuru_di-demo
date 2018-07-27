fee9163173a180bd7f640418344d714c571fb1ef
tolgatuna <tolga.tuna@etstur.com>
2018-07-27 17:06:04 +0300

    82. Using Factory Beans

--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -1,12 +1,5 @@
 package guru.springframework.services;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
-@Service
-@Primary
-@Profile({"de", "default"}) //German Primary Profile
 public class PrimaryGermanGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
